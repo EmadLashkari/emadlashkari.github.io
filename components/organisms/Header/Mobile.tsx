b31247d2d6f29f2bc46352bd0190eb43f7bb459f
Emad Lashkari <emadlashkari.82@gmail.com>
2025-02-25 22:43:02 +0330

    fix build error

--- components/organisms/Header/Mobile.tsx
+++ components/organisms/Header/Mobile.tsx
@@ -2,6 +2,6 @@ import React, { useState } from "react";
 
 export default function MobileHeader() {
   const [name, setName] = useState<number>(0);
-  useMemo(() => first, [second])
+
   return <div>this header for mobile</div>;
 }
