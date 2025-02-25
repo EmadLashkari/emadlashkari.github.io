99c50fb648cbd42ad0b30a16860f011389c54744
Emad Lashkari <emadlashkari.82@gmail.com>
2025-02-25 22:30:59 +0330

    adding background

---
+++ components/organisms/Background/Animated-Background.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { FlipWords } from "@/components/atoms/FlipWord";
+import { AuroraBackground } from "@/components/ui/aurora-background";
+import { motion } from "framer-motion";
+import React from "react";
+
+export function AnimatedBackground() {
+  return (
+    <AuroraBackground>
+      <div className="w-full h-full flex flex-col justify-around items-center">
+        <motion.div
+          initial={{ opacity: 0.0, y: 40 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{
+            delay: 0.3,
+            duration: 0.8,
+            ease: "easeInOut",
+          }}
+          className="relative flex flex-col gap-8 items-center justify-center px-4"
+        >
+          <div className="text-3xl md:text-7xl font-bold text-white text-center">
+            Hi 👋 This Emad Lashkari
+          </div>
+          <div className="text-base font-light flex flex-col justify-center items-center gap-3 md:text-4xl dark:text-neutral-200 py-4">
+            <span>Creative Developer That Love</span>
+            <FlipWords words={["Beautifull", "Responsive", "Easy To Use"]} />
+            <span>Apps</span>
+          </div>
+        </motion.div>
+        <video
+          autoPlay
+          loop
+          muted
+          preload="auto"
+          playsInline
+          className="text-white z-[10000] w-14 h-14"
+        >
+          <source src="/anim/MouseScroll.webm" type="video/webm" />
+        </video>
+      </div>
+    </AuroraBackground>
+  );
+}
