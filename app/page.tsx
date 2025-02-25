e40d1ade274ef989f6c64fbec5d99157630f0ec1
Emad Lashkari <emadlashkari.82@gmail.com>
2025-02-25 22:30:41 +0330

    change and add animation background

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
-import Header from "@/components/Header/Header";
-import Intro from "@/components/pages/Landing/Intro";
-import ProfileImg from "@/components/pages/Landing/ProfileImg";
+import { AnimatedBackground } from "@/components/organisms/Background/Animated-Background";
 
 export default function Home() {
   return (
     <main className="relative bg-darkSmoke w-full h-[100vh]">
-      <Header />
-      <ProfileImg />
-      <Intro />
-      <section></section>
+      <AnimatedBackground />
     </main>
   );
 }
