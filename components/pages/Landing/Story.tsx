cd3143efae20c163fce3c6b9da32664645b55cee
Emad Lashkari <emadlashkari.82@gmail.com>
2025-04-21 20:22:43 +0330

    add new sections

---
+++ components/pages/Landing/Story.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Story() {
+  return (
+    <span className="text-white font-agelia">
+      I TELL EVERY UNIQE STORY YOU HAVE NOT MATTER IS BIG SUCCESSFULL OR JUST A
+      SIMPLE ONE.
+    </span>
+  );
+}
---
+++ components/pages/Landing/TheJourney.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+
+export default function TheJourney() {
+  return (
+    <div className=" text-white text-4xl absolute top-[50%] left-[50%] -translate-x-[50%]">
+      <span className="second-page-text font-bolden text-9xl">
+        THE JOURNEY HAS BEGIN
+      </span>
+    </div>
+  );
+}
