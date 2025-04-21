eaeb86c13ee64c7577069d2ba5b8c1a73e4ac0a0
Emad Lashkari <emadlashkari.82@gmail.com>
2025-04-21 20:22:34 +0330

    add animation

--- components/organisms/ScrollWraper/ScrollWraper.tsx
+++ components/organisms/ScrollWraper/ScrollWraper.tsx
@@ -1,20 +1,51 @@
 "use client";
 import React, { ReactElement } from "react";
-import { motion, useScroll, useSpring } from "motion/react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-function ScrollWraper({ children }: { children: ReactElement[] }) {
-  const { scrollYProgress } = useScroll();
-  const scaleX = useSpring(scrollYProgress, {
-    stiffness: 100,
-    damping: 30,
-    restDelta: 0.001,
-  });
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 
-  return (
-    <div className="w-full h-screen overflow-y-scroll snap-y snap-mandatory">
-      {children}
-    </div>
-  );
+function ScrollWraper({
+  children,
+}: {
+  children: ReactElement[] | ReactElement;
+}) {
+  useGSAP(() => {
+    // Create GSAP timeline with ScrollTrigger
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".first-page",
+        start: "top top",
+        end: "bottom top",
+        scrub: 2,
+        pin: true,
+        pinSpacing: false,
+        markers: true, // Set to true for debugging
+      },
+    });
+
+    // Timeline animations
+    tl.to(".half-circle", {
+      height: "100vh",
+      yPercent: -100,
+      width: "100%",
+      ease: "power2.out",
+      duration: 1,
+    }).to(
+      ".second-page-text",
+      {
+        scaleY: 1,
+        y: 1,
+        height: 10,
+        duration: 0.3,
+        ease: "elastic.out(1, 0.3)",
+      },
+      "-=0.3"
+    );
+  }, []);
+
+  return <div className="w-full h-full ">{children}</div>;
 }
 
 export default ScrollWraper;
