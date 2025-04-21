f032924e2363b6d45753c799cd599733173c2481
Emad Lashkari <emadlashkari.82@gmail.com>
2025-04-21 20:22:27 +0330

    add navbar

--- components/organisms/Navbar/navbar.tsx
+++ components/organisms/Navbar/navbar.tsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import { HoveredLink, Menu, MenuItem, ProductItem } from "./navbar-menu";
 import { cn } from "@/lib/utils/cn";
+import Link from "next/link";
 
 export default function Navbar({ className }: { className?: string }) {
   const [active, setActive] = useState<string | null>(null);
@@ -9,12 +10,18 @@ export default function Navbar({ className }: { className?: string }) {
     <div
       className={cn("fixed top-5 inset-x-0 max-w-2xl mx-auto z-50", className)}
     >
-      <Menu setActive={setActive}>
-        <HoveredLink href="/web-dev">Products</HoveredLink>
+      {/* <Menu setActive={setActive}> */}
+      {/* <HoveredLink href="/web-dev">Products</HoveredLink>
         <HoveredLink href="/interface-design">About</HoveredLink>
         <HoveredLink href="/seo">Prices</HoveredLink>
-        <HoveredLink href="/branding">ContactMe</HoveredLink>
-      </Menu>
+        <HoveredLink href="/branding">ContactMe</HoveredLink> */}
+      <Link className="text-white" href="/web-dev">
+        Products
+      </Link>
+      <Link href="/interface-design">About</Link>
+      <Link href="/seo">Prices</Link>
+      <Link href="/branding">ContactMe</Link>
+      {/* </Menu> */}
     </div>
   );
 }
