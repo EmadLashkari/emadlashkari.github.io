7f742776358f997c306ba57d81368ddddcec3be9
Emad Lashkari <emadlashkari.82@gmail.com>
2025-06-02 23:38:42 +0330

    the intro text animation

---
+++ components/atoms/Intro.tsx
@@ -0,0 +1,19 @@
+"use client";
+import { motion } from "motion/react";
+
+export default function IntroText() {
+  return (
+    <motion.h1
+      initial={{ opacity: 0.5, y: 100 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{
+        delay: 0.3,
+        duration: 0.8,
+        ease: "easeInOut",
+      }}
+      className=" bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
+    >
+      Emad Lashkari <br /> Creative Developer
+    </motion.h1>
+  );
+}
