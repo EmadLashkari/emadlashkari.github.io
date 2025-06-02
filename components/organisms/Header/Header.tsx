78bcd4df409cc6a2bf0b418a483fe47ea28d7612
Emad Lashkari <emadlashkari.82@gmail.com>
2025-06-02 23:39:17 +0330

    change header

--- components/organisms/Header/Header.tsx
+++ components/organisms/Header/Header.tsx
@@ -3,13 +3,8 @@ import React from "react";
 
 export default function Header() {
   return (
-    <header className="w-full flex flex-row justify-between  items-center text-white gap-5 text-2xl font-bold">
-      <div className="flex flex-row justify-end px-10 items-center text-white gap-5 text-2xl font-bold">
-        <Link href={""}>Home</Link>
-        <Link href={""}>About Me</Link>
-        <Link href={""}>Projects</Link>
-        <Link href={""}>Blog</Link>{" "}
-      </div>
+    <header className="p-3 fixed w-full z-50 flex flex-row justify-between  items-center text-white gap-5  font-bold">
+      <h1 className="font-stacker text-white">Emad Lashkari</h1>
     </header>
   );
 }
