aecdba78e8590e4dff3c9c0d4b279674df556c62
Emad Lashkari <emadlashkari.82@gmail.com>
2025-10-22 00:32:50 +0330

    add about me and project sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,23 @@
 import IntroText from "@/components/atoms/Intro";
+import Header from "@/components/organisms/Header/Header";
 import { LampContainerGsap } from "@/components/organisms/LightBulb/lamp-gsap";
 import Timeline from "@/components/organisms/Timeline/Timeline";
+import AboutMe from "@/components/pages/Landing/AboutMe";
+import Projects from "@/components/pages/Landing/Projects";
+import { SmoothAnimationProvider } from "@/lib/context/SmoothAnimation";
 
 export default function Home() {
   return (
-    <main className="w-full h-full overflow-hidden relative">
+    <main className=" w-full h-full overflow-hidden relative bg-slate-950">
+      {/* <SmoothAnimationProvider> */}
+      <Header />
       <LampContainerGsap>
         <IntroText />
       </LampContainerGsap>
-
+      <Projects />
+      <AboutMe />
       <Timeline />
+      {/* </SmoothAnimationProvider> */}
     </main>
   );
 }
