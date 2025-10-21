df6edb3c0aca8545f3ed1379a095c0a586b144e8
Emad Lashkari <emadlashkari.82@gmail.com>
2025-10-22 00:33:16 +0330

    change headline text

--- components/atoms/Intro.tsx
+++ components/atoms/Intro.tsx
@@ -13,7 +13,9 @@ export default function IntroText() {
       }}
       className=" bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
     >
-      Emad Lashkari <br /> Creative Developer
+      Code with Purpose
+      <br />
+      Design with Impact
     </motion.h1>
   );
 }
