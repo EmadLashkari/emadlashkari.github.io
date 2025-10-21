4663e4b6814589ba5ce522fd084ee45c67852271
Emad Lashkari <emadlashkari.82@gmail.com>
2025-10-22 00:34:21 +0330

    add about me section

---
+++ components/pages/Landing/AboutMe.tsx
@@ -0,0 +1,22 @@
+export default function AboutMe() {
+  return (
+    <div id="AboutMe" className="p-4 md:p-10 flex flex-col gap-3">
+      <h1 className="tracking-tight text-transparent bg-clip-text bg-gradient-to-br from-slate-300 to-slate-500 text-4xl">
+        About Me
+      </h1>
+      <p className="pb-10 tracking-tight text-transparent bg-clip-text bg-gradient-to-br from-slate-300 to-slate-500">
+        Ever since I was a kid, I haven't just loved playing video games—I've
+        been obsessed with how they're made. That curiosity is what first led me
+        to programming. I spent two years diving in headfirst, learning
+        everything I could. While I quickly realized that building a full game
+        was a huge challenge, I fell in love with the process of building
+        itself. I shifted my focus to web development, originally with a simple
+        goal: to earn enough for a better PC. But along the way, I discovered a
+        real passion for creating things that live on the web. That was over
+        three years ago, and I've been hooked ever since. My journey might have
+        started with game engines, but it's led me to a career I truly love,
+        building fast, beautiful, and user-friendly websites.
+      </p>
+    </div>
+  );
+}
