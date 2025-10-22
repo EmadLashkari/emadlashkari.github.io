2a0d6f8529afc9eab3d40f0121f1be8ff2a2a9ce
Emad Lashkari <emadlashkari.82@gmail.com>
2025-10-22 10:43:51 +0330

    fix the text

--- components/pages/Landing/AboutMe.tsx
+++ components/pages/Landing/AboutMe.tsx
@@ -5,17 +5,18 @@ export default function AboutMe() {
         About Me
       </h1>
       <p className="pb-10 tracking-tight text-transparent bg-clip-text bg-gradient-to-br from-slate-300 to-slate-500">
-        Ever since I was a kid, I haven't just loved playing video games—I've
-        been obsessed with how they're made. That curiosity is what first led me
-        to programming. I spent two years diving in headfirst, learning
-        everything I could. While I quickly realized that building a full game
-        was a huge challenge, I fell in love with the process of building
-        itself. I shifted my focus to web development, originally with a simple
-        goal: to earn enough for a better PC. But along the way, I discovered a
-        real passion for creating things that live on the web. That was over
-        three years ago, and I've been hooked ever since. My journey might have
-        started with game engines, but it's led me to a career I truly love,
-        building fast, beautiful, and user-friendly websites.
+        Ever since I was a kid, I haven&apos;t just loved playing video
+        games—I&apos;ve been obsessed with how they&apos;re made. That curiosity
+        is what first led me to programming. I spent two years diving in
+        headfirst, learning everything I could. While I quickly realized that
+        building a full game was a huge challenge, I fell in love with the
+        process of building itself. I shifted my focus to web development,
+        originally with a simple goal: to earn enough for a better PC. But along
+        the way, I discovered a real passion for creating things that live on
+        the web. That was over three years ago, and I&apos;ve been hooked ever
+        since. My journey might have started with game engines, but it&apos;s
+        led me to a career I truly love, building fast, beautiful, and
+        user-friendly websites.
       </p>
     </div>
   );
